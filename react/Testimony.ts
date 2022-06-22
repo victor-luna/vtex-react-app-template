a05d95f6f8a62d18c3cbae1cd56b49aa878d6940
victor-luna <victor.vazguimaraes@gmail.com>
2022-06-22 11:18:56 -0300

    added only one custom element, added store/interfaces.json to export component, updated manifest.json

---
+++ react/Testimony.ts
@@ -0,0 +1,3 @@
+import Testimony from './components/Testimony/index'
+
+export default Testimony
---
+++ react/components/Testimony/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Testimony = () => {
+  return (
+    <>
+      <h1 className="flex items-center justify-center">Testimony</h1>
+    </>
+  )
+}
+
+export default Testimony
