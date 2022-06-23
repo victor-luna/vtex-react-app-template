644b0a6fd2ca3b9fe314f334a43d348ace38b406
victor-luna <victor.vazguimaraes@gmail.com>
2022-06-23 12:06:58 -0300

    custom component created

--- react/components/Testimony/index.tsx
+++ react/components/Testimony/index.tsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import TestimonialCard from 'material-testimonial-card'
 
 const Testimony = () => {
   return (
     <>
-      <h1 className="flex items-center justify-center">Testimony</h1>
+      <TestimonialCard
+        name="John Smith"
+        image="https://i.ibb.co/Ks9npcQ/albert-dera-ILip77-Sbm-OE-unsplash.jpg"
+        content="Wow! This testimonial card is so amazing! I would like to use it in my project!"
+        project="Testimonial card"
+      />
+
+      <TestimonialCard
+        name="John Smith"
+        image="https://i.ibb.co/3rsWj49/allef-vinicius-C-1jj-FJio-Wg-unsplash.jpg"
+        content="Wow! This testimonial card is so amazing! I would like to use it in my project!"
+        project="Testimonial card"
+      />
+
+      <TestimonialCard
+        name="John Smith"
+        image="https://i.ibb.co/x6FSd4z/yingchou-han-IJr-Ie-Cs3-D4g-unsplash.jpg"
+        content="Wow! This testimonial card is so amazing! I would like to use it in my project!"
+        project="Testimonial card"
+      />
     </>
   )
 }
